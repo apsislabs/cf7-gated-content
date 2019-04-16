82695f3d86a3b050cd904ae3d54f94f2562732a5
Wyatt Kirby <wyatt@apsis.io>
2019-04-16 10:01:03 -0700

    Update JS to better handle error cases

--- js/cf7_gated_content.js
+++ js/cf7_gated_content.js
@@ -1,23 +1,46 @@
 (function($) {
-    document.addEventListener(
-        "wpcf7submit",
-        function(event) {
-            document.cookie =
-                wpcf7gc.cookieKey + event.detail.contactFormId + "=1";
+  var SUCCESS_STATUS = "mail_sent";
+  var AJAX_ACTION = "getDownloadButton";
 
-            jQuery.ajax({
-                type: "post",
-                dataType: "json",
-                url: wpcf7gc.ajaxurl,
-                data: {
-                    action: "getDownloadButton",
-                    contactFormId: event.detail.contactFormId
-                },
-                success: function(res) {
-                    jQuery(event.target).append(res.data);
-                }
-            });
+  document.addEventListener(
+    "wpcf7submit",
+    function(event) {
+      var detail = event.detail;
+      var apiResponse = detail.apiResponse;
+
+      // bail early if we didn't succeed
+      if (apiResponse.status !== SUCCESS_STATUS) {
+        return;
+      }
+
+      var $target = $(apiResponse.into);
+      var downloadId = "wpcf7-download-" + detail.unitTag;
+
+      // Set a cookie
+      document.cookie = wpcf7gc.cookieKey + detail.contactFormId + "=1";
+
+      $.ajax({
+        type: "post",
+        dataType: "json",
+        url: wpcf7gc.ajaxurl,
+        data: {
+          action: AJAX_ACTION,
+          contactFormId: detail.contactFormId
         },
-        false
-    );
+        error: function(res) {
+          console.error("Error Fetching Gated Content: ", res);
+          alert(
+            "There was an error fetching your download. Please refresh the page."
+          );
+        },
+        success: function(res) {
+          if ($target && $("#" + downloadId).length < 1) {
+            var $downloadContainer = $("<div></div>", { id: downloadId });
+            $downloadContainer.append(res.data).appendTo($target);
+          }
+        }
+      });
+    },
+    false
+  );
 })(jQuery);
