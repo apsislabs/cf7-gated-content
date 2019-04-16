e87b046ee2727387d81c3bb18d0756c5aee0eea8
Wyatt Kirby <wyatt@apsis.io>
2019-04-16 16:02:32 -0700

    Update version numbers

--- cf7_gated_content.php
+++ cf7_gated_content.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name: Contact Form 7 Gated Content
  * Description: An add-on for Contact Form 7 that allows you to gate content behind form submission
- * Version: 1.1.1
+ * Version: 1.2.0
  * Author: Apsis Labs
  * Author URI: http://apsis.io
  * License: GPLv3
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Tags: form, contact form, contact form 7, cf7, gated content, gated file
 Requires at least: 4.5
 Tested up to: 5.1.1
 Requires PHP: 5.6
-Stable tag: 1.1.1
+Stable tag: 1.2.0
 License: GPLv2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
