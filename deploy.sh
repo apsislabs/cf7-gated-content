356dfa406a0f49748b3358966cfcac7d60417bbf
Wyatt Kirby <wyatt@apsis.io>
2019-04-17 09:32:01 -0700

    Fix deployment issue

--- deploy.sh
+++ deploy.sh
@@ -95,7 +95,7 @@ svn commit --username=$SVNUSER -m "$COMMITMSG"
 echo "Changing directory to SVN assets and committing"
 cd $SVNPATH/assets/
 # Add all new files that are not set to be ignored
-svn status | grep -v "^.[ \t]*\..*" | grep "^?" | awk '{print $2}' | xargs svn add
+svn add --force * --auto-props --parents --depth infinity -q
 svn commit --username=$SVNUSER -m "$COMMITMSG"
 
 echo "Creating new SVN tag & committing it"
